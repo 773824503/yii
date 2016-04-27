1881c56413e31efbaf877e0a1873cdf1c1be0bb5
773824503 <773824503@qq.com>
2016-04-27 05:48:08 -0700

    update

--- basic/controllers/HomeController.php
+++ basic/controllers/HomeController.php
@@ -8,13 +8,16 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\myfunction\Method;
 
 class HomeController extends Controller
 {
 
     public function actionMy()
     {
-        echo "hello world";die;
+        $method = new Method();
+        $res = $method->add(30,30);
+        echo "hello world  ".$res;die;
         return $this->render('index');
     }
 }
